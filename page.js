c9a100597c0260b8237ccb424ac33953f4f2d09e
yjq <yjq@yjqdeMacBook-Pro.local>
2018-08-13 09:08:30 +0800

    add 更新deploy 方式

--- page.js
+++
@@ -1,8 +0,0 @@
-var ghpages = require('gh-pages');
- 
-ghpages.publish('doc/.vuepress/dist', {
-    branch: 'gh-pages',
-    repo: 'git@github.com:xiaoqiang730730/vue_apicloud.git'
-}, function(err) {
-    console.log(err);
-});
